45a6c53108791fb538445446006fe08a1ff81bca
Mukul Rajput <mukulrajput0707@gmail.comt>
2025-12-06 14:14:48 +0530

    STL

---
+++ MultiMap.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int explainMultiMap(){
+    // everything is same as map, only it can store multiple keys
+    // only map[keys] cannot be stored used here
+
+    return 0;
+}
